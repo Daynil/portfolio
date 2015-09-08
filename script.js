3d2e53373cbed5c1058783dfa85479812b64bdd4
Danny Libin <omegasol11@gmail.com>
2015-09-08 18:31:27 -0500

    fixed background resizing issue by specifying a min-height to encompass all main-page items

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    
+
+    // Smooth scroll on link click
     var $root = $('html, body');
     $('a[href*=#]').click(function() {
-        /*alert("top position: " + $( $(this).attr('href') ).position().top);*/
         var scrollToPos = $( $(this).attr('href') ).offset().top
         $root.animate({
             scrollTop: scrollToPos
